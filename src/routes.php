16b3127dfd94a320a8566d330f000407449f2505
Djang0 <ludovic.reenaers@gmail.com>
2016-03-11 13:30:06 +0100

    getHits

--- src/routes.php
+++ src/routes.php
@@ -21,17 +21,50 @@ $app->get('/dump/', function ($request, $response, $args) {
     $response = $response->withHeader('Content-type', 'application/json');
     $body = $response->getBody();
     $data = $request->getHeaders();
-    
+
     //$data['id']='2';
     $body->write(json_encode($data));
 });
+
+$app->get('/getHits/{target_id}', function ($request, $response, $args) {
+
+    $response = $response->withHeader('Content-type', 'application/json');
+    $body = $response->getBody();
+    try {
+        $target_id = $args['target_id'];
+                                //header('Location: http://www.new-website.com/', true, 301);
+                                //exit();
+
+        $db = getDB();
+        $sth = $db->prepare('select * from hits');
+        //$sth->bindParam(':code', $code, PDO::PARAM_STR);
+
+        $sth->execute();
+        $target = $sth->fetch(PDO::FETCH_OBJ);
+
+        if ($target) {
+            //$ipAddress = $request->getAttribute('ip_address');
+            //$referrer = $request->getHeaderLine('HTTP_REFERER');
+
+            $response = $response->withStatus(200);
+            $body->write(json_encode($target));
+        } else {
+            $response = $response->withStatus(200);
+            $body->write('{"hits":{"msg":"none"}}');
+        }
+        $db = null;
+    } catch (PDOException $e) {
+        $response->withStatus(404);
+        $body->write('{"error":{"msg":'.$e->getMessage().'}}');
+    }
+});
+
 //keep referer in params
 $app->get('/from/{code}', function ($request, $response, $args) {
 
     $response = $response->withHeader('Content-type', 'text/html');
     $body = $response->getBody();
     try {
-        
         $code = $args['code'];
                                 //header('Location: http://www.new-website.com/', true, 301);
                                 //exit();
@@ -39,25 +72,26 @@ $app->get('/from/{code}', function ($request, $response, $args) {
         $db = getDB();
         $sth = $db->prepare('select * from targets where code = :code');
         $sth->bindParam(':code', $code, PDO::PARAM_STR);
-        
-        $sth->execute();
-		$target = $sth->fetch(PDO::FETCH_OBJ);
 
-                            if ($target) {
-                            	$ipAddress = $request->getAttribute('ip_address');
-								$referrer = $request->getHeaderLine('HTTP_REFERER');
-                         		$sth = $db->prepare('insert into hits (target_id, ip,referrer) VALUES (:target_id, :ip, :ref)');
-        						$sth->bindParam(':target_id', $target->id, PDO::PARAM_INT);
-        						$sth->bindParam(':ip', $ipAddress, PDO::PARAM_STR);
-        						$sth->bindParam(':ref', $referrer, PDO::PARAM_STR);
-        						$sth->execute();
-        						
-                                $response = $response->withStatus(301);
-                                header('Location: '.$target->url.'?referrer='.urlencode($referrer), true, 301);
-                            } else {
-                                $response = $response->withStatus(404);
-                                return $this->renderer->render($response, 'index.phtml', $args);
-                            }
+        $sth->execute();
+        $target = $sth->fetch(PDO::FETCH_OBJ);
+
+        if ($target) {
+            $ipAddress = $request->getAttribute('ip_address');
+            $referrer = $request->getHeaderLine('HTTP_REFERER');
+            $sth = $db->prepare('insert into hits (target_id, ip,referrer) VALUES (:target_id, :ip, :ref)');
+            $sth->bindParam(':target_id', $target->id, PDO::PARAM_INT);
+            $sth->bindParam(':ip', $ipAddress, PDO::PARAM_STR);
+            $sth->bindParam(':ref', $referrer, PDO::PARAM_STR);
+            $sth->execute();
+
+            $response = $response->withStatus(301);
+            header('Location: '.$target->url.'?referrer='.urlencode($referrer), true, 301);
+        } else {
+            $response = $response->withStatus(404);
+
+            return $this->renderer->render($response, 'index.phtml', $args);
+        }
         $db = null;
     } catch (PDOException $e) {
         $response->withStatus(404);
@@ -69,7 +103,6 @@ $app->get('/to/{code}', function ($request, $response, $args) {
     $response = $response->withHeader('Content-type', 'text/html');
     $body = $response->getBody();
     try {
-        
         $code = $args['code'];
                                 //header('Location: http://www.new-website.com/', true, 301);
                                 //exit();
@@ -77,24 +110,25 @@ $app->get('/to/{code}', function ($request, $response, $args) {
         $db = getDB();
         $sth = $db->prepare('select * from targets where code = :code');
         $sth->bindParam(':code', $code, PDO::PARAM_STR);
-        
-        $sth->execute();
-		$target = $sth->fetch(PDO::FETCH_OBJ);
 
-                            if ($target) {
-                            	$ipAddress = $request->getAttribute('ip_address');
-								$referrer = $request->getHeaderLine('HTTP_REFERER');
-                         		$sth = $db->prepare('insert into hits (target_id, ip,referrer) VALUES (:target_id, :ip, :ref)');
-        						$sth->bindParam(':target_id', $target->id, PDO::PARAM_INT);
-        						$sth->bindParam(':ip', $ipAddress, PDO::PARAM_STR);
-        						$sth->bindParam(':ref', $referrer, PDO::PARAM_STR);
-        						$sth->execute();
-                                $response = $response->withStatus(301);
-                                header('Location: '.$target->url, true, 301);
-                            } else {
-                                $response = $response->withStatus(404);
-                                return $this->renderer->render($response, 'index.phtml', $args);
-                            }
+        $sth->execute();
+        $target = $sth->fetch(PDO::FETCH_OBJ);
+
+        if ($target) {
+            $ipAddress = $request->getAttribute('ip_address');
+            $referrer = $request->getHeaderLine('HTTP_REFERER');
+            $sth = $db->prepare('insert into hits (target_id, ip,referrer) VALUES (:target_id, :ip, :ref)');
+            $sth->bindParam(':target_id', $target->id, PDO::PARAM_INT);
+            $sth->bindParam(':ip', $ipAddress, PDO::PARAM_STR);
+            $sth->bindParam(':ref', $referrer, PDO::PARAM_STR);
+            $sth->execute();
+            $response = $response->withStatus(301);
+            header('Location: '.$target->url, true, 301);
+        } else {
+            $response = $response->withStatus(404);
+
+            return $this->renderer->render($response, 'index.phtml', $args);
+        }
         $db = null;
     } catch (PDOException $e) {
         $response->withStatus(404);
